73c52327c04d05a00caf6c58b2a1fcc0d62b370e
postolenko <denis.postolenko@gmail.com>
2020-03-14 01:53:44 +0200

    Lavanda

--- js/scripts.js
+++ js/scripts.js
@@ -23,32 +23,31 @@ $(document).scroll(function() {
 
 $(document).ready(function() {
 
-    if( $(".portfolio_slider").length > 0 ) {
-        $(".portfolio_slider").not(".slick-initialized").slick({
+    if( $(".testimonial_slider").length > 0 ) {
+        $(".testimonial_slider").not(".slick-initialized").slick({
             dots: true,
             arrows: true,
-            autoplay: true,
+            // autoplay: true,
             autoplaySpeed: 4000,
             speed: 1200,
-            slidesToShow: 4,
+            slidesToShow: 2,
             slidesToScroll: 1,
-            // fade: true,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 540,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
         });
     }
 
